6801926beb02535bf8f51c20eb2d6d1c19a07f0e
Aiden Shi <aidenhohoho@gmail.com>
2020-05-29 14:45:41 -0700

    Added new shapes

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 export class Paintr {
   constructor(canvasID) {
-  
     this.canvas = document.getElementById(canvasID);
-      if (this.canvas.getContext) {
-          var ctx = canvas.getContext('2d');
-     }
+    if (this.canvas.getContext) {
+      this.ctx = this.canvas.getContext("2d");
+    }
+    this.color = "#000000";
   }
-
+  setColor(color) {
+    this.color = color;
+  }
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  rect(x, y, width, height, color = this.color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(x, y, width, height);
+  }
+  startPath(startX, startY) {
+    this.ctx.beginPath();
+    this.ctx.moveTo(startX, startY);
+  }
+  line(startX, startY, endX, endY, color = this.color) {
+    this.ctx.fillStyle = color;
+    this.ctx.beginPath();
+    this.ctx.moveTo(startX, startY);
+    this.ctx.lineTo(endX, endY);
+    this.ctx.stroke();
+  }
+  ellipse(x, y, width, height, color = this.color) {
+    this.ctx.arc(x, y);
+  }
+  circle(x, y, radius, color = this.color) {
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  }
 }
