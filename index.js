a46f3aabb1f612c5fbe94e6af2921c0cc569e78a
vooxal <58456527+vooxal@users.noreply.github.com>
2020-05-29 13:37:12 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports.Paintr = class Paintr {
+  constructor(canvasID) {
+  
+    this.canvas = document.getElementById(canvasID);
+      if (this.canvas.getContext) {
+          var ctx = canvas.getContext('2d');
+     }
+  }
+  init(){
+  }
+}
+module.exports.Paintr;
